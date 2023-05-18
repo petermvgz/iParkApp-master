0fadcf4245d014db54b48c234941eaff13b8f812
peter <109552816+petermvgz@users.noreply.github.com>
2023-05-18 14:23:23 -0700

    Added an Alert to the "pay" button that lets the user confirm or cancel their parking spot reservation

--- iParkApp/DetailView/PaymentView.swift
+++ iParkApp/DetailView/PaymentView.swift
@@ -10,21 +10,33 @@ import SwiftUI
 struct PaymentView: View {
     @Binding var selectedHour: CGFloat
     let perHourFee: CGFloat
+    @State private var showingConfirmationAlert = false
+    
     var body: some View {
         HStack {
             Text("$\(String.init(format: "%.2f", selectedHour/2 * perHourFee))")
                 .font(.system(size: 22, weight: .bold))
             Spacer()
             Button(action: {
-                //
+                showingConfirmationAlert = true
             }, label: {
                 Text("Pay")
                     .font(.system(size: 20, weight: .semibold))
-                    .foregroundColor(.yellowColor)
+                    .foregroundColor(.yellow)
                     .frame(width: 180, height: 60)
-                    .background(Color.purpleColor)
+                    .background(Color.purple)
                     .cornerRadius(20)
             })
+        }
+        .alert(isPresented: $showingConfirmationAlert) {
+            Alert(
+                title: Text("Confirmation"),
+                message: Text("Confirm parking space?"),
+                primaryButton: .default(Text("Confirm"), action: {
+                    // Perform actions when the user confirms
+                }),
+                secondaryButton: .cancel(Text("Cancel"))
+            )
         }
     }
 }
